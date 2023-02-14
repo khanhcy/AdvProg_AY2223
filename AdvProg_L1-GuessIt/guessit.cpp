2505d65fd1e78a178f0d674a5ee68b64b74a9cf8
khanhcy <quockhanhcy@gmail.com>
2023-02-14 15:16:22 +0700

    sua loi

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -87,7 +87,6 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-    bool result = false;
     if (isContinued == 'y' || isContinued == 'Y') result = true;
     return result;
 }
